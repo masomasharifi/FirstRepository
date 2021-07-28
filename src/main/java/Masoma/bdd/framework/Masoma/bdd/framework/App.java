9e9bbab956e510bd5ba59e0408872f50efc1f579
masomasharifi <massharifi@gmail.com>
2021-07-28 14:18:37 -0700

    This is my first commit

---
+++ src/main/java/Masoma/bdd/framework/Masoma/bdd/framework/App.java
@@ -0,0 +1,13 @@
+package Masoma.bdd.framework.Masoma.bdd.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
